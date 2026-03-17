c7e9c4143b875c64368220bc5b02a7987aede277
connor hoehn <connorhoehn@gmail.com>
2026-03-17 13:00:51 -0400

    feat(27-02): mount groupMembersRouter in central router
    - Import groupMembersRouter from './group-members'
    - Mount at /groups/:groupId so mergeParams exposes req.params.groupId

--- social-api/src/routes/index.ts
+++ social-api/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { profilesRouter } from './profiles';
 import { socialRouter } from './social';
 import { groupsRouter } from './groups';
+import { groupMembersRouter } from './group-members';
 
 const router = Router();
 
 router.use('/profiles', profilesRouter);
 router.use('/social', socialRouter);
 router.use('/groups', groupsRouter);
+router.use('/groups/:groupId', groupMembersRouter);
 
 export default router;
